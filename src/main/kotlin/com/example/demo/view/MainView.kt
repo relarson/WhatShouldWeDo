eee5f4de8ae92952d6a7e0ee9c79b46ad42c9e11
Ross Larson <roslarso@amazon.com>
2019-03-11 21:41:16 -0700

    Add basic tag and location textfields. non functional

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -4,6 +4,7 @@ import com.example.demo.model.Activity
 import com.example.demo.model.Season
 import com.example.demo.model.Seasons
 import com.example.demo.model.TripLength
+import javafx.beans.property.SimpleStringProperty
 import javafx.scene.control.ToggleGroup
 import tornadofx.*
 import java.time.LocalDate
@@ -20,6 +21,8 @@ class MainView : View("What Should We Do v0.1") {
     private var seasonFilter: Seasons = Seasons()
     private var maxLength : TripLength = TripLength.WEEKEND
     private var maxTravel : Int = 0
+    private var tags : String = ""
+    private val locations = SimpleStringProperty("")
 
     // groups
     private val lengthGroup = ToggleGroup()
@@ -27,118 +30,140 @@ class MainView : View("What Should We Do v0.1") {
 
     override val root = borderpane {
         setPrefSize(1000.0, 400.0)
-        top = hbox {
-            vbox {
-                label("Desired Seasons")
-                hbox {
-                    togglebutton("Spring") {
-                        action {
-                            seasonFilter.apply {
-                                if (isSelected) addSeason(Season.SPRING) else removeSeason(Season.SPRING)
+        top = vbox {
+            hbox {
+                vbox {
+                    label("Desired Seasons")
+                    hbox {
+                        togglebutton("Spring") {
+                            action {
+                                seasonFilter.apply {
+                                    if (isSelected) addSeason(Season.SPRING) else removeSeason(Season.SPRING)
+                                }
+                                tableData.refilter()
                             }
-                            tableData.refilter()
+                            isSelected = false
                         }
-                        isSelected = false
+                        togglebutton("Summer") {
+                            action {
+                                seasonFilter.apply {
+                                    if (isSelected) addSeason(Season.SUMMER) else removeSeason(Season.SUMMER)
+                                }
+                                tableData.refilter()
+                            }
+                            isSelected = false
+                        }
+                        togglebutton("Autumn") {
+                            action {
+                                seasonFilter.apply {
+                                    if (isSelected) addSeason(Season.AUTUMN) else removeSeason(Season.AUTUMN)
+                                }
+                                tableData.refilter()
+                            }
+                            isSelected = false
+                        }
+                        togglebutton("Winter") {
+                            action {
+                                seasonFilter.apply {
+                                    if (isSelected) addSeason(Season.WINTER) else removeSeason(Season.WINTER)
+                                }
+                                tableData.refilter()
+                            }
+                            isSelected = false
+                        }
                     }
-                    togglebutton("Summer") {
-                        action {
-                            seasonFilter.apply {
-                                if (isSelected) addSeason(Season.SUMMER) else removeSeason(Season.SUMMER)
-                            }
-                            tableData.refilter()
-                        }
-                        isSelected = false
-                    }
-                    togglebutton("Autumn") {
-                        action {
-                            seasonFilter.apply {
-                                if (isSelected) addSeason(Season.AUTUMN) else removeSeason(Season.AUTUMN)
-                            }
-                            tableData.refilter()
-                        }
-                        isSelected = false
-                    }
-                    togglebutton("Winter") {
-                        action {
-                            seasonFilter.apply {
-                                if (isSelected) addSeason(Season.WINTER) else removeSeason(Season.WINTER)
-                            }
-                            tableData.refilter()
-                        }
-                        isSelected = false
+                    hboxConstraints {
+                        marginRight = 20.0
                     }
                 }
-                hboxConstraints {
-                    marginRight = 20.0
+                vbox {
+                    label("Max Trip Length")
+                    hbox {
+                        togglebutton("Half-Day", lengthGroup) {
+                            action {
+                                maxLength = if (isSelected) TripLength.HALF_DAY else TripLength.WEEKEND
+                                tableData.refilter()
+                            }
+                            isSelected = false
+                        }
+                        togglebutton("Day Trip", lengthGroup) {
+                            action {
+                                maxLength = if (isSelected) TripLength.DAY else TripLength.WEEKEND
+                                tableData.refilter()
+                            }
+                            isSelected = false
+                        }
+                        togglebutton("Weekend", lengthGroup) {
+                            action {
+                                maxLength = TripLength.WEEKEND
+                                tableData.refilter()
+                            }
+                            isSelected = false
+                        }
+                    }
+                    hboxConstraints {
+                        marginRight = 20.0
+                    }
+                }
+                vbox {
+                    label("Max Travel Time")
+                    hbox {
+                        togglebutton("30m", distanceGroup) {
+                            action {
+                                maxTravel = if (isSelected) 30 else 0
+                                tableData.refilter()
+                            }
+                            isSelected = false
+                        }
+                        togglebutton("1hr", distanceGroup) {
+                            action {
+                                maxTravel = if (isSelected) 60 else 0
+                                tableData.refilter()
+                            }
+                            isSelected = false
+                        }
+                        togglebutton("2hr", distanceGroup) {
+                            action {
+                                maxTravel = if (isSelected) 120 else 0
+                                tableData.refilter()
+                            }
+                            isSelected = false
+                        }
+                        togglebutton("3hr", distanceGroup) {
+                            action {
+                                maxTravel = if (isSelected) 180 else 0
+                                tableData.refilter()
+                            }
+                            isSelected = false
+                        }
+                        togglebutton(">3hr", distanceGroup) {
+                            action {
+                                maxTravel = 0
+                                tableData.refilter()
+                            }
+                            isSelected = false
+                        }
+                    }
+                }
+                vboxConstraints {
+                    marginTop = 5.0
+                    marginLeft = 5.0
+                    marginBottom = 5.0
                 }
             }
-            vbox {
-                label("Max Trip Length")
-                hbox {
-                    togglebutton("Half-Day", lengthGroup) {
-                        action {
-                            maxLength = if (isSelected) TripLength.HALF_DAY else TripLength.WEEKEND
-                            tableData.refilter()
-                        }
-                        isSelected = false
-                    }
-                    togglebutton("Day Trip", lengthGroup) {
-                        action {
-                            maxLength = if (isSelected) TripLength.DAY else TripLength.WEEKEND
-                            tableData.refilter()
-                        }
-                        isSelected = false
-                    }
-                    togglebutton("Weekend", lengthGroup) {
-                        action {
-                            maxLength = TripLength.WEEKEND
-                            tableData.refilter()
-                        }
-                        isSelected = false
+            hbox {
+                textfield(tags) {
+                    promptText = "Desired tag(s) ..."
+                    hboxConstraints {
+                        marginRight = 20.0
                     }
                 }
-                hboxConstraints {
-                    marginRight = 20.0
+                textfield(locations) {
+                    promptText = "Desired location(s) ..."
                 }
-            }
-            vbox {
-                label("Max Travel Time")
-                hbox {
-                    togglebutton("30m", distanceGroup) {
-                        action {
-                            maxTravel = if (isSelected) 30 else 0
-                            tableData.refilter()
-                        }
-                        isSelected = false
-                    }
-                    togglebutton("1hr", distanceGroup) {
-                        action {
-                            maxTravel = if (isSelected) 60 else 0
-                            tableData.refilter()
-                        }
-                        isSelected = false
-                    }
-                    togglebutton("2hr", distanceGroup) {
-                        action {
-                            maxTravel = if (isSelected) 120 else 0
-                            tableData.refilter()
-                        }
-                        isSelected = false
-                    }
-                    togglebutton("3hr", distanceGroup) {
-                        action {
-                            maxTravel = if (isSelected) 180 else 0
-                            tableData.refilter()
-                        }
-                        isSelected = false
-                    }
-                    togglebutton(">3hr", distanceGroup) {
-                        action {
-                            maxTravel = 0
-                            tableData.refilter()
-                        }
-                        isSelected = false
-                    }
+                vboxConstraints {
+                    marginLeft = 5.0
+                    marginBottom = 5.0
                 }
             }
         }
